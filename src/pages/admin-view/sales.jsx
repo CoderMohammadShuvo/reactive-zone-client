fccbd62b6173ae7aea19b1eec30d2ac43f64bc40
Shagor-Mia <infinityshagor18557@gmail.com>
2025-02-26 15:54:15 +0600

    sale for client

---
+++ src/pages/admin-view/sales.jsx
@@ -0,0 +1,11 @@
+import AdminSalesView from "@/components/admin-view/sales";
+
+function AdminSales() {
+  return (
+    <div>
+      <AdminSalesView />
+    </div>
+  );
+}
+
+export default AdminSales;
